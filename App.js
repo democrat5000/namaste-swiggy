d2bd90bd2557cfe573a70776f5f2c1051621a2f9
democrat5000 <democrat5000@gmail.com>
2025-05-17 11:49:54 +0100

    nested functional components

--- App.js
+++ App.js
@@ -1,22 +1,50 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const e = React.createElement
-const r = ReactDOM.createRoot(document.getElementById("root"))
+const Heading1 = ()=> {
+  return(<h1 className="heading1" style={{fontSize:"4rem", padding:"5px"}}>I am an h1 element</h1>)
+}
 
-const parent = e("div", {className:"parent"},[
-  e("div", {key:"1", className:"child1"},
-    [e("h1", {key:"5"}, "I am an h1 element"),
-      e("h2", {key:"6"}, "I am an h2 element")],
-      e("div", {key:"2", className:"child2"},
-    [e("h1", {key:"7"}, "I am an h1 element"),
-      e("h2", {key:"8"}, "I am an h2 element")]
-  ))],
-e("div", {key:"3", className:"child3"},
-    [e("h1", {key:"9"}, "I am an h1 element"),
-      e("h2", {key:"10"}, "I am an h2 element")],
-      e("div", {key:"4", className:"child4"},
-    [e("h1", {key:"11"}, "I am an h1 element"),
-      e("h2", {key:"12"}, "I am an h2 element")])))
+const Heading2 = ()=> {
+  return(<h2 className="heading2"  style={{fontSize:"2rem", padding:"5px"}}>I am an h2 element</h2>)
+}
 
-  r.render(parent)
+const Child1 = ()=> {
+  return(<div className="child1" style={{backgroundColor:"purple" , fontStyle:"italic"}}>
+    <Heading1/>
+    <Heading2/>
+  </div>)
+}
+
+const Child2 = ()=> {
+  return(<div className="child2" style={{backgroundColor:"brown" , fontStyle:"italic"}}>
+    <Heading1/>
+    <Heading2/>
+  </div>)
+}
+
+const Child3 = ()=> {
+  return(<div className="child3" style={{backgroundColor:"green" , fontStyle:"italic"}}>
+    <Heading1/>
+    <Heading2/>
+  </div>)
+}
+
+const Child4 = ()=> {
+  return(<div className="child4" style={{backgroundColor:"tomato" , fontStyle:"italic"}}>
+    <Heading1/>
+    <Heading2/>
+  </div>)
+}
+
+const Parent = ()=> {
+  return(<div className="parent">
+    <Child1/>
+    <Child2/>
+    <Child3/>
+    <Child4/>
+  </div>)
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Parent/>)
