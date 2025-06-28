35472b19c23afa0132550b643204f70e7ba8c5b9
democrat5000 <democrat5000@gmail.com>
2025-06-28 13:18:22 +0100

    to master

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -10,14 +10,14 @@ const SocialLinks = ()=> {
     <div className="social">
       <h2 style={{fontWeight:"bold"}}>Social Links</h2>
       <div className="social-links">
-      <a target="_blank" href={LINKEDIN_URL} target="_blank"><img src={LINKEDIN_SRC_URL}
+      <a target="_blank" href={LINKEDIN_URL}><img src={LINKEDIN_SRC_URL}
         className="linkedin"></img></a>
 
         <a target="_blank" href={FACEBOOK_URL}><img src={FACEBOOK_SRC_URL} 
         className="facebook"
         ></img></a>
 
-        <a href={TWITTER_URL} target="_blank"><img src={TWITTER_SRC_URL} 
+        <a target="_blank" href={TWITTER_URL}><img src={TWITTER_SRC_URL} 
         className="twitter"
         ></img>
 
